b437c13d252f8f503126538bb6f858bb0fa7bfb4
Gabriel Godoi <80178149+gabrielcad@users.noreply.github.com>
2024-03-06 16:12:15 +0000

    aula3

---
+++ ALURAFUNDAMENTOS.js
@@ -0,0 +1,84 @@
+// tipo Number
+
+// const meuNumero = 3;
+
+// const primeiroNumero = 15;
+// const segundoNumero = 20;
+
+// const operacaoMatematica = primeiroNumero * segundoNumero;
+
+// console.log(operacaoMatematica)
+
+// // ponto flutuante
+
+// const numeroPontoFlutuante = 3.3;
+
+// const pontoFlutuanteSemZero = .5;
+
+// const novaOperacao = primeiroNumero / numeroPontoFlutuante * pontoFlutuanteSemZero;
+
+// console.log(novaOperacao)
+
+// NaN -> Not A Number (nao e um numero)
+
+// const alura = 'Alura';
+
+// console.log(alura * primeiroNumero)
+
+////
+
+//class
+
+// class painel {
+//     constructor(descricao, wolts){
+//     this.descricao = descricao;
+//     this.wolts = wolts;
+//     }    
+// }
+// class inversor {
+//     constructor(descricao, fase){
+//         this.descricao = descricao;
+//         this.fase = fase;
+//     }
+// }
+
+//  painel1 = new painel("Painel de","550w")
+//  painel2 = new painel("Painel de","650w")
+//  inversor1 = new inversor("inversor Odex", "trifásico")
+
+// console.log(inversor1)
+// console.log(painel1)
+// // 
+
+//STRING
+
+//Tudo que colocar dentro das "" o JS irá considerar como string caracteres + números
+
+// const texto1 = "Olá, mundo!";
+// const texto2 = 'Olá, mundo!';
+// const senha = "senhaSuperSegura456!";
+// const StringDeNumeros = "3456";
+
+// const citacao = "Meu nome é ";
+// const meuNome = "Leonardo";
+
+//concatenação (+)
+
+//console.log(citacao + meuNome)
+
+//template string OU template literal
+
+//Bollean
+//true ->verdadeiro
+// false -> falso
+
+const primeiroNumero = 5;
+const segundoNumero = 5;
+let cadastroAtivado = true;
+
+// console.log(primeiroNumero === segundoNumero);
+
+const texto1 = "Alura";
+const texto2 = "Alura.";
+
+console.log (texto1 === texto2)
