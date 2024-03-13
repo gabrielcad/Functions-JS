676d11d7ddba1e855f1481cf3f92029091e465f2
Gabriel Godoi <80178149+gabrielcad@users.noreply.github.com>
2024-03-13 16:42:53 +0000

    aula-template-string

---
+++ template-string.js
@@ -0,0 +1,13 @@
+const nome = "Gabriel";
+const sobrenome = "Alessandro";
+const idade = "26 anos";
+
+
+const apresentacao = "meu nome é " + nome +" " + sobrenome + " minha idade é " +  idade;
+
+// const apresentacao = `meu nome é ${nome}, meu sobrenome é ${sobrenome}, minha idade é ${idade}`;
+
+console.log(apresentacao)
+
+
+//3ª forma de escrever string 1ª'',2ª"" e 3ª ${}... 
