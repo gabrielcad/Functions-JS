bb86abd3c5e419d0d4bc2f399f28424f1086981b
Gabriel Godoi <80178149+gabrielcad@users.noreply.github.com>
2024-03-13 15:56:35 +0000

    aula ternario

---
+++ ternario.js
@@ -0,0 +1,13 @@
+const idadeMinima = 18;
+const idadeCliente = 19;
+
+// if (idadeCliente >= idadeMinima){
+//     console.log("Liberado para logar e selecionar qualquer tipo de bebida.")
+// } else{
+//     console.log("Liberado para escolher apenas bebidas sem alcool!")
+// }
+            //condição                        //true                    //false     
+console.log(idadeCliente >= idadeMinima ? "bebidas com alcool" : "bebidas sem alcool")
+
+//tabela de produto deve conter a coluna "Contem_alcool = S ou N".
+
