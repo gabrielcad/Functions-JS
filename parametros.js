66bfe1b8897c8462978a7dbec5f0bdcafbbd5a30
Gabriel Godoi <80178149+gabrielcad@users.noreply.github.com>
2024-03-17 15:10:07 +0000

    arrow, function

--- parametros.js
+++ parametros.js
@@ -1,27 +1,40 @@
- function soma(){
-          return 2 + 2;
-  }
+//  function soma(){
+//           return 2 + 2;
+//   }
 
- function somaOutroNumero(){
-     return 50 + 25;
-}
+//  function somaOutroNumero(){
+//      return 50 + 25;
+// }
 
-console.log(soma() + somaOutroNumero())
+// console.log(soma() + somaOutroNumero())
 
-// parametros de função
+// // parametros de função
 
-function soma(numero1, numero2){
+// function soma(numero1, numero2){
+//     return numero1 + numero2;
+// }
+
+// console.log(soma(2, 2))
+// console.log(soma(245, 22))
+// console.log(soma(-9852, 153))
+
+// // parametros x argumentos
+
+// // ordem dos parametros
+
+// function nomeIdade(nome, idade){
+//    return `meu nome é ${nome} e minha idade é ${idade} `; 
+// }
+
+// console.log(nomeIdade(26, "Gabriel"))
+
+function soma(numero1, numero2) {
     return numero1 + numero2;
 }
 
-console.log(soma(2, 2))
-console.log(soma(245, 22))
-console.log(soma(-9852, 153))
-
-// parametros x argumentos
-
-// ordem dos parametros
-
-function nomeIdade(nome, idade){
-    
+function multiplica(numero1, numero2) {
+    return numero1 * numero2;
 }
+
+                            //9         //6   
+console.log(multiplica(soma(4, 5), soma(3, 3)))
